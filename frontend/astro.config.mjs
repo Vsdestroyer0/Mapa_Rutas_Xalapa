88246cc9ff99e9cc05d5867680e3015366194989
Vsdestroyer0 <ggmendoza2005@gmail.com>
2025-10-14 21:12:28 -0600

    commit ver el puerto

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -14,8 +14,8 @@ export default defineConfig({
   vite: {
     server: {
       proxy: {
-        "/api": 'http://localhost:3000',
-        /*  '/api': 'https://backend-mapa-production.up.railway.app', */
+        /*"/api": 'http://localhost:3000',*/
+        '/api': 'https://backend-mapa-production-1dc7.up.railway.app',
       },
     },
     plugins: [tailwindcss()]
